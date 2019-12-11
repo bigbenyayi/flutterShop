4e6a37f6c91a4bdeb991f1c5a462cf4d55e92fa8
Benjamin Corben <corbenbenjamin@gmail.com>
2019-12-11 19:21:36 +0100

    Alert Dialogs

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -31,6 +31,27 @@ class CartItem extends StatelessWidget {
           vertical: 4,
         ),
       ),
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+              title: Text("Are you sure?"),
+              content: Text("Do you want to remove the item from cart?"),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(false);
+                  },
+                  child: Text("No"),
+                ),FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(true);
+                  },
+                  child: Text("Yes"),
+                ),
+              ]),
+        );
+      },
       direction: DismissDirection.endToStart,
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
